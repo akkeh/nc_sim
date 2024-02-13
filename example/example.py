9e444842becdf2a5fa94db523862c7c375ca1349
akke <akkemats.houben@ub.edu>
2024-02-13 22:10:25 +0100

    update example (added comments)

--- example/example.py
+++ example/example.py
@@ -3,17 +3,22 @@ import matplotlib.pyplot as plt
 
 import nc_sim
 
-width = 1
-height = 1
-H = np.random.rand(10,10)
+width = 1   # width of culture
+height = 1  # height of culture
+H = np.random.rand(10,10)   # obstacles
 
+# place neurons on plane:
 X, Y = nc_sim.axons.place_neurons(width, height, H, rho=100)
 
+# grow axons:
 W = nc_sim.axons.grow_W(width, height, X, Y, H=H)
 
+# initialise neurons:
 nw = nc_sim.neurons.izhikevich.init_network(len(X), W, Tn=20e3, sigma=2.25)
 
+# run dynamics:
 nc_sim.neurons.izhikevich.run_nw(nw)
 
+# plot:
 plt.plot(nw['spikes_T'], nw['spikes_Y'], '.')
 plt.show()
